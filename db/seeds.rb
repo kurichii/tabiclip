f90652430cf27a35ba2c176ec424a3f55a0e4f79
kurichii <ayaka.takahashi830@gmail.com>
2025-01-11 17:00:55 +0900

    add:areaレコード用seeds.rb編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+ApplicationRecord.transaction do
+areas = [
+  { id: 1, name: "Hokkaido" },
+  { id: 2, name: "Tohoku" },
+  { id: 3, name: "KitaKanto" },
+  { id: 3, name: "Syutoken" },
+  { id: 4, name: "Koshinetsu" },
+  { id: 4, name: "Tokai" },
+  { id: 5, name: "Hokuriku" },
+  { id: 5, name: "Kinki" },
+  { id: 6, name: "SaninSanyou" },
+  { id: 7, name: "Shikoku" },
+  { id: 8, name: "Kyushu" },
+  { id: 9, name: "Okinawa" }
+]
+
+  areas.each do |area|
+    Area.find_or_create_by(id: area[:id]) do |a|
+      a.name = area[:name]
+    end
+  end
+end
