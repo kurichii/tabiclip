86b72d164b6ecef1db245032d9d29da07ee8c308
kurichii <ayaka.takahashi830@gmail.com>
2025-01-11 23:16:09 +0900

    add:travel_typeレコード追加とarea用コードの微修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Areasテーブルに値を格納
 ApplicationRecord.transaction do
 areas = [
   { id: 1, name: "Hokkaido" },
   { id: 2, name: "Tohoku" },
   { id: 3, name: "KitaKanto" },
-  { id: 3, name: "Syutoken" },
-  { id: 4, name: "Koshinetsu" },
-  { id: 4, name: "Tokai" },
-  { id: 5, name: "Hokuriku" },
-  { id: 5, name: "Kinki" },
-  { id: 6, name: "SaninSanyou" },
-  { id: 7, name: "Shikoku" },
-  { id: 8, name: "Kyushu" },
-  { id: 9, name: "Okinawa" }
+  { id: 4, name: "Syutoken" },
+  { id: 5, name: "Koshinetsu" },
+  { id: 6, name: "Tokai" },
+  { id: 7, name: "Hokuriku" },
+  { id: 8, name: "Kinki" },
+  { id: 9, name: "SaninSanyou" },
+  { id: 10, name: "Shikoku" },
+  { id: 11, name: "Kyushu" },
+  { id: 12, name: "Okinawa" },
+  { id: 13, name: "other" }
 ]
 
   areas.each do |area|
     Area.find_or_create_by(id: area[:id]) do |a|
       a.name = area[:name]
+    end
+  end
+
+  # TravelerTypesテーブルに値を格納
+  traveler_types = [
+    { id: 1, name: "family" },
+    { id: 2, name: "friends" },
+    { id: 3, name: "alone" },
+    { id: 4, name: "couple" },
+    { id: 5, name: "other" }
+  ]
+
+  traveler_types.each do | traveler_type |
+    TravelerType.find_or_create_by(id: traveler_type[:id]) do |t|
+      t.name = traveler_type[:name]
     end
   end
 end
