d36abae4f37a61680d1929d033cacf0c30fff0d7
kurichii <ayaka.takahashi830@gmail.com>
2025-01-11 11:21:15 +0900

    add:しおり作成機能追加

--- app/controllers/travel_books_controller.rb
+++ app/controllers/travel_books_controller.rb
@@ -1,9 +1,25 @@
 class TravelBooksController < ApplicationController
   def index
-    @travel_books = TravelBook.all
+    @travel_books = TravelBook.includes(:users)
   end
 
   def new
     @travel_book = TravelBook.new
+  end
+
+  def create
+    @travel_book = current_user.travel_books.build(travel_book_param)
+    if @travel_book.save
+      redirect_to travel_books_path, success: "作成成功"
+    else
+      flash.now[:danger] = "作成失敗"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def travel_book_param
+    params.require(:travel_book).permit(:title, :description, :is_public, :start_date, :end_date)
   end
 end
