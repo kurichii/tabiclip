69a5ac666b20f1d7c234717003e30d4c79129a60
kurichii <ayaka.takahashi830@gmail.com>
2025-01-13 16:03:39 +0900

    add:デフォルト画像の設定

--- app/uploaders/travel_book_uploader.rb
+++ app/uploaders/travel_book_uploader.rb
@@ -21,6 +21,9 @@ class TravelBookUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+  def default_url
+    "travel_book_placeholder"
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
