fd2af966d63d182cafe40ac6692146e468c241a6
kurichii <ayaka.takahashi830@gmail.com>
2025-01-17 16:47:50 +0900

    update:現在のパスに応じてactiveクラス付与を判断する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 module ApplicationHelper
   # 指定したパスが現在のページであれば"active"を返す
   def add_active_class(path)
-    "active" if current_page?(path)
+    if path == travel_books_path && (request.fullpath == root_path || request.fullpath == travel_books_path)
+      "active"
+    elsif request.fullpath == path
+      "active"
+    else
+      ""
+    end
   end
 end
