15c099f3cfa6755df0322d09bc474e1783b6d120
kurichii <ayaka.takahashi830@gmail.com>
2025-01-17 18:28:08 +0900

    add:ボトムナビゲーションのビュー作成と表示するビューの条件分岐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def set_travel_book
+    @travel_book = TravelBook.find(params[:id])
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  # ボトムナビゲーションのだしわけ
+  # TravelController#showでユーザーがしおりとリレーションを組んでいる場合true
+  def display_bottom_nav?
+    controller_name == "travel_books" && action_name == "show" && current_user.travel_books.exists?(id: params[:id])
+  end
+
   # 指定したパスが現在のページであれば"active"を返す
   def add_active_class(path)
     if path == travel_books_path && (request.fullpath == root_path || request.fullpath == travel_books_path)
