3e941717150d11e6b82631a0fa535f44f0265a98
kurichii <ayaka.takahashi830@gmail.com>
2025-01-22 12:41:50 +0900

    add:scheduleモデルにバリデーション追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,8 @@
 class Schedule < ApplicationRecord
   belongs_to :travel_book
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :memo, length: { maximum: 65_535 }
+  validates :budged, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :end_date, comparison: { greater_than: :start_date }
 end
