f4848ad7ceb471b523e13ade770ef85482fb6879
kurichii <ayaka.takahashi830@gmail.com>
2025-01-23 10:50:48 +0900

    add:スケジュール一覧のビュー生成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,6 @@
 class SchedulesController < ApplicationController
   def index
-    travel_book = current_user.travel_books.find(params[:travel_book_id])
-    @schedules = travel_book.schedules
+    @travel_book = current_user.travel_books.find(params[:travel_book_id])
+    @schedules = @travel_book.schedules
   end
 end
