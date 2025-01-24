8f992092d0be663f494d4f6a63cec2cce000c30e
kurichii <ayaka.takahashi830@gmail.com>
2025-01-24 11:34:59 +0900

    update:日付変換はapplication_helperで行うように修正

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -3,10 +3,6 @@ module SchedulesHelper
     schedules.sum { | schedule | schedule.budged.to_i }
   end
 
-  def fmt_date(date)
-    date.strftime("%Y年%-m月%-d日(%a)") || ""
-  end
-
   def fmt_datetime(date)
     date.strftime("%H:%M") || "未定"
   end
