6f1d7c68aa9d95bd3acae6a6b674d2f83afa4042
kurichii <ayaka.takahashi830@gmail.com>
2025-01-24 10:18:09 +0900

    add:コントローラーにnew,createアクション定義

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,27 @@
 class SchedulesController < ApplicationController
   def index
     @travel_book = current_user.travel_books.find(params[:travel_book_id])
-    @schedules = @travel_book.schedules
+    @schedules = @travel_book.schedules.sort_by(&:start_date)
+  end
+
+  def new
+    @travel_book = current_user.travel_books.find(params[:travel_book_id])
+    @schedule = @travel_book.schedules.new
+  end
+
+  def create
+    @travel_book = current_user.travel_books.find(params[:travel_book_id])
+    @schedule = @travel_book.schedules.build(schedule_param)
+    if @schedule.save
+      redirect_to travel_book_schedules_path(@travel_book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def schedule_param
+    params.require(:schedule).permit(:title, :budged, :memo, :start_date, :end_date)
   end
 end
