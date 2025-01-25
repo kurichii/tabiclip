6e675d67cdb1bf7f38a42d91ce967d32f672b9c1
kurichii <ayaka.takahashi830@gmail.com>
2025-01-25 19:05:21 +0900

    update:end_dateにstart_dateよりも前の日付を入れられないようにバリデーション追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,5 +4,13 @@ class Schedule < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :memo, length: { maximum: 65_535 }
   validates :budged, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :end_date, comparison: { greater_than: :start_date }
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    if start_date.present? && end_date.present? && end_date < start_date
+      errors.add(:end_date, "must be after start date")
+    end
+  end
 end
--- app/models/travel_book.rb
+++ app/models/travel_book.rb
@@ -10,11 +10,20 @@ class TravelBook < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 65_535 }
+  validate :end_date_after_start_date
 
   scope :public_travel_books, -> { where(is_public: true) }
 
   def owned_by_user?(user)
     return false if user.nil?
     users.exists?(id: user.id)
+  end
+
+  private
+
+  def end_date_after_start_date
+    if start_date.present? && end_date.present? && end_date < start_date
+      errors.add(:end_date, "must be after start date")
+    end
   end
 end
