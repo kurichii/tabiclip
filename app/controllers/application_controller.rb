a57816f3b12a58a2275f15919180cf309ef3372b
kurichii <ayaka.takahashi830@gmail.com>
2025-02-13 10:28:27 +0900

    add:flashメッセージの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  add_flash_types :success, :error
 
   def set_travel_book
     @travel_book = TravelBook.find(params[:id])
