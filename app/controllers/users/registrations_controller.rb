9e0ca104fbfe95c9ef81124587c34f4acfdcb83e
kurichii <ayaka.takahashi830@gmail.com>
2025-02-13 22:03:26 +0900

    add:アカウント登録後とログイン後のリダイレクト先を設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -54,10 +54,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :icon_image, :icon_image_cache ])
   end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  # アカウント登録後のリダイレクト先
+  def after_sign_up_path_for(resource)
+    travel_books_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
