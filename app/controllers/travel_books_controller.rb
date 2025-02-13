1b6ae9cdf14d7cb1a97f20ab67cda949e3229bbe
kurichii <ayaka.takahashi830@gmail.com>
2025-02-13 11:23:53 +0900

    update:flashメッセージの文言修正

--- app/controllers/travel_books_controller.rb
+++ app/controllers/travel_books_controller.rb
@@ -21,9 +21,9 @@ class TravelBooksController < ApplicationController
     if @travel_book.save
       # 中間テーブルに関連付けを追加
       current_user.user_travel_books.create(travel_book: @travel_book)
-      redirect_to travel_books_path, notice: "作成成功"
+      redirect_to travel_books_path, success: t("defaults.flash_message.created", item: TravelBook.model_name.human)
     else
-      flash.now[:alert] = "作成失敗"
+      flash.now[:error] = t("defaults.flash_message.not_created", item: TravelBook.model_name.human)
       render :new, status: :unprocessable_entity
     end
   end
@@ -40,9 +40,9 @@ class TravelBooksController < ApplicationController
     @travel_book = current_user.travel_books.find(params[:id])
 
     if @travel_book.update(travel_book_param)
-      redirect_to travel_books_path(@travel_book), notice: "編集成功"
+      redirect_to travel_books_path(@travel_book), success: t("defaults.flash_message.updated", item: TravelBook.model_name.human)
     else
-      flash.now[:alert] = "編集失敗"
+      flash.now[:error] = t("defaults.flash_message.not_updated", item: TravelBook.model_name.human)
       render :edit, status: :unprocessable_entity
     end
   end
@@ -50,14 +50,14 @@ class TravelBooksController < ApplicationController
   def destroy
     travel_book = current_user.travel_books.find(params[:id])
     travel_book.destroy!
-    redirect_to travel_books_path, notice: "削除成功"
+    redirect_to travel_books_path, success: t("defaults.flash_message.deleted", item: TravelBook.model_name.human)
   end
 
   def delete_image
     @travel_book = TravelBook.find(params[:id])
     @travel_book.remove_travel_book_image! # CarrierWaveのメソッドを使って画像を削除
     @travel_book.save
-    redirect_to edit_travel_book_path(@travel_book), notice: "削除しました"
+    redirect_to edit_travel_book_path(@travel_book), success: t("defaults.flash_message.deleted", item: TravelBook.model_name.human)
   end
 
   private
