127215d0301cd077eb5a86c4fc8be5ac2eb0f2cd
kurichii <ayaka.takahashi830@gmail.com>
2025-02-14 13:24:43 +0900

    update:ログアウト後のリダイレクト先をtop画面に指定

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,6 +24,12 @@ class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     travel_books_path
   end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource)
+    home_index_path
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
