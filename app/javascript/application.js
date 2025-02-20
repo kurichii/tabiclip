bd6dff6e8c2f3877302a628dc940dd93a9c56f5c
kurichii <ayaka.takahashi830@gmail.com>
2025-02-20 18:55:46 +0900

    update:リファクタリング

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-
-document.addEventListener("turbo:load", () => {
-  // mapの表示領域か場所名のinputがある場合のみinitMapを実行
-  let map = document.getElementById("map")
-  const inputSpotName = document.getElementById("spotName");
-  if(map || inputSpotName){
-    initMap();
-  }
-});
