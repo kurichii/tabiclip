c3b9f75c03f64c645160aa9cb7b661cd2d54d701
kurichii <ayaka.takahashi830@gmail.com>
2025-02-21 14:56:47 +0900

    add:check_listsテーブルにuuidカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_21_041915) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_21_055118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_21_041915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "travel_book_uuid", null: false
+    t.uuid "uuid", default: -> { "gen_random_uuid()" }, null: false
     t.index ["travel_book_uuid"], name: "index_check_lists_on_travel_book_uuid"
+    t.index ["uuid"], name: "index_check_lists_on_uuid", unique: true
   end
 
   create_table "list_items", force: :cascade do |t|
