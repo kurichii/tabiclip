ad64ef288e4aa44c6cab5173e14e1fba7b1687de
kurichii <ayaka.takahashi830@gmail.com>
2025-02-21 14:31:25 +0900

    fix:Lintチェックエラー解消

--- db/migrate/20250221021131_add_travel_book_uuid_foreign_key.rb
+++ db/migrate/20250221021131_add_travel_book_uuid_foreign_key.rb
@@ -5,13 +5,13 @@ class AddTravelBookUuidForeignKey < ActiveRecord::Migration[7.2]
     add_column :user_travel_books, :travel_book_uuid, :uuid
 
     Schedule.reset_column_information
-    Schedule.find_each { |s| s.update_column(:travel_book_uuid, TravelBook.find(s.travel_book_id).uuid)}
+    Schedule.find_each { |s| s.update_column(:travel_book_uuid, TravelBook.find(s.travel_book_id).uuid) }
 
     CheckList.reset_column_information
-    CheckList.find_each { |c| c.update_column(:travel_book_uuid, TravelBook.find(c.travel_book_id).uuid)}
+    CheckList.find_each { |c| c.update_column(:travel_book_uuid, TravelBook.find(c.travel_book_id).uuid) }
 
     UserTravelBook.reset_column_information
-    UserTravelBook.find_each { |ut| ut.update_column(:travel_book_uuid, TravelBook.find(ut.travel_book_id).uuid)}
+    UserTravelBook.find_each { |ut| ut.update_column(:travel_book_uuid, TravelBook.find(ut.travel_book_id).uuid) }
 
     change_column_null :schedules, :travel_book_uuid, false
     change_column_null :check_lists, :travel_book_uuid, false
