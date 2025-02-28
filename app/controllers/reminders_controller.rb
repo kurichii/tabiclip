8e3ede8d262d537ca88c840e1e43061eeaedd139
kurichii <ayaka.takahashi830@gmail.com>
2025-02-28 16:12:45 +0900

    fix:ReminderControllerのレンダリングの部分をビューに移行

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -3,20 +3,12 @@ class RemindersController < ApplicationController
     @list_item = ListItem.find(params[:list_item_id])
     @check_list = @list_item.check_list
     @reminder = @list_item.build_reminder(reminder_params)
-      if @reminder.save
-        flash.now.notice = t("defaults.flash_message.created", item: Reminder.model_name.human)
-        render turbo_stream: [
-          # turbo_stream.remove("reminder_modal"),
-          turbo_stream.replace("list_item_#{@list_item.id}", partial: "list_items/list_item", locals: { list_item: @list_item, reminder: @reminder }),
-          turbo_stream.update("flash_messages", partial: "shared/flash_message")
-        ]
-      else
-        # flash.now.alert = t("defaults.flash_message.not_created", item: Reminder.model_name.human)
-        render turbo_stream: [
-          turbo_stream.update("reminder_form_#{@list_item.id}", partial: "reminders/form", locals: { list_item: @list_item, reminder: @reminder }),
-          turbo_stream.update("flash_messages", partial: "shared/flash_message"),
-        ]
-      end
+
+    if @reminder.save
+      flash.now[:notice] = t("defaults.flash_message.created", item: Reminder.model_name.human)
+    else
+      render "not_create"
+    end
   end
 
   def update
@@ -25,20 +17,9 @@ class RemindersController < ApplicationController
     @check_list = @list_item.check_list
 
     if @reminder.update(reminder_params)
-      flash.now.notice = t("defaults.flash_message.updated", item: Reminder.model_name.human)
-      render turbo_stream: [
-        # turbo_stream.remove("reminder_modal"),
-        turbo_stream.replace("list_item_#{@list_item.id}", partial: "list_items/list_item", locals: { list_item: @list_item, reminder: @reminder }),
-        turbo_stream.update("flash_messages", partial: "shared/flash_message")
-      ]
+      flash.now[:notice] = t("defaults.flash_message.updated", item: Reminder.model_name.human)
     else
-      # flash.now.alert = t("defaults.flash_message.not_updated", item: Reminder.model_name.human)
-      render turbo_stream: [
-        turbo_stream.update("reminder_form_#{@list_item.id}", partial: "reminders/form", locals: { list_item: @list_item, reminder: @reminder }),
-        turbo_stream.update("flash_messages", partial: "shared/flash_message"),
-        # turbo_stream.replace("modal", partial: "reminders/modal", locals: { list_item: @list_item, reminder: @reminder }),
-        # turbo_stream.update("flash_messages", partial: "shared/flash_message")
-      ]
+      render "not_update"
     end
   end
 
