e49c1a002765ecc7f0a195eb32a64491b2ed41dd
kurichii <ayaka.takahashi830@gmail.com>
2025-03-01 16:18:06 +0900

    update:reminder_dateのnull許容後、list_itemの作成でbuild_reminderされるようにしたためcreateアクションを削除

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,23 +1,11 @@
 class RemindersController < ApplicationController
-  def create
-    @list_item = ListItem.find(params[:list_item_id])
-    @check_list = @list_item.check_list
-    @reminder = @list_item.build_reminder(reminder_params)
-
-    if @reminder.save
-      flash.now[:notice] = t("flash_message.reminder.created", item: Reminder.model_name.human)
-    else
-      render "not_create"
-    end
-  end
-
   def update
     @reminder = Reminder.find(params[:id])
     @list_item = @reminder.list_item
     @check_list = @list_item.check_list
 
     if @reminder.update(reminder_params)
-      flash.now[:notice] = t("defaults.flash_message.updated", item: Reminder.model_name.human)
+      flash.now[:notice] = t("flash_message.reminder.updated", item: Reminder.model_name.human)
     else
       render "not_update"
     end
@@ -27,9 +15,9 @@ class RemindersController < ApplicationController
     @reminder = Reminder.find(params[:id])
     @list_item = @reminder.list_item
     if @reminder.update(reminder_date: nil)
-      flash.now[:notice] = t("flash_message.reminder.canceled")
+      flash.now[:notice] = t("flash_message.reminder.canceled", item: Reminder.model_name.human)
     else
-      flash.now[:alert] = t("flash_message.reminder.not_canceled")
+      flash.now[:alert] = t("flash_message.reminder.not_canceled", item: Reminder.model_name.human)
     end
   end
 
