157cd2402ca556a8c08fa783e3b74efd5c177968
kurichii <ayaka.takahashi830@gmail.com>
2025-03-01 15:05:38 +0900

    fix:reminder_dateのバリデーション削除

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,7 +1,6 @@
 class Reminder < ApplicationRecord
   belongs_to :list_item, optional: true
 
-  validates :reminder_date, presence: true
   validate :reminder_date_cannot_be_in_the_past
 
   def reminder_date_cannot_be_in_the_past
