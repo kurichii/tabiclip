1f90a8e255ad5c249637127acb7f0476f34c40ee
kurichii <ayaka.takahashi830@gmail.com>
2025-03-03 17:02:45 +0900

    add:sidekiqのサーバーとクライアントにredisサーバーへの接続設定

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+# Sideliqのサーバーとクライアントが同じRedisサーバーに接続するように設定
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://redis:6379" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://redis:6379" }
+end
