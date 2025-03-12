55938032764ebc4b2b5dbe5ef2d8d7d6ac7c5d27
kurichii <ayaka.takahashi830@gmail.com>
2025-03-12 15:33:57 +0900

    fix:Lintチェックエラー解消と不要な設定削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,9 +44,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
+    address: "smtp.gmail.com",
     domain: "smtp.gmail.com",
-    port:587,
+    port: 587,
     user_name: ENV["MAILER_SENDER"],
     password: ENV["MAILER_PASSWORD"],
     authentication: :login,
