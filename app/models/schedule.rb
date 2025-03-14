4225d8b5f6913aa00a0d4d5a83f2cef3bb0c192e
kurichii <ayaka.takahashi830@gmail.com>
2025-03-14 13:14:57 +0900

    add:ScheduleとScheduleIconモデルの関連付け

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,7 @@
 class Schedule < ApplicationRecord
   belongs_to :travel_book, foreign_key: :travel_book_uuid
   has_one :spot, primary_key: :uuid, foreign_key: :schedule_uuid, dependent: :destroy
+  belongs_to :schedule_icon, optional: true
 
   def self.group_by_date(schedules)
     schedules.group_by { |schedule| (schedule.start_date&.to_date || schedule.end_date&.to_date) || "日付未定" }
