a6e28cf94e46f1fc7f7af0f718fc4b0291535b44
kurichii <ayaka.takahashi830@gmail.com>
2025-03-18 18:26:20 +0900

    fix:flash_messageのtypeをsuccessからnoticeへ修正

--- app/controllers/travel_books_controller.rb
+++ app/controllers/travel_books_controller.rb
@@ -16,7 +16,7 @@ class TravelBooksController < ApplicationController
     if @travel_book.save
       # 中間テーブルに関連付けを追加
       current_user.user_travel_books.create(travel_book: @travel_book)
-      redirect_to travel_books_path, success: t("defaults.flash_message.created", item: TravelBook.model_name.human)
+      redirect_to travel_books_path, notice: t("defaults.flash_message.created", item: TravelBook.model_name.human)
     else
       flash.now[:alert] = t("defaults.flash_message.not_created", item: TravelBook.model_name.human)
       render :new, status: :unprocessable_entity
@@ -35,7 +35,7 @@ class TravelBooksController < ApplicationController
     @travel_book = current_user.travel_books.find(params[:id])
 
     if @travel_book.update(travel_book_param)
-      redirect_to travel_books_path(@travel_book), success: t("defaults.flash_message.updated", item: TravelBook.model_name.human)
+      redirect_to travel_books_path(@travel_book), notice: t("defaults.flash_message.updated", item: TravelBook.model_name.human)
     else
       flash.now[:alert] = t("defaults.flash_message.not_updated", item: TravelBook.model_name.human)
       render :edit, status: :unprocessable_entity
@@ -45,7 +45,7 @@ class TravelBooksController < ApplicationController
   def destroy
     travel_book = current_user.travel_books.find(params[:id])
     travel_book.destroy!
-    redirect_to travel_books_path, success: t("defaults.flash_message.deleted", item: TravelBook.model_name.human)
+    redirect_to travel_books_path, notice: t("defaults.flash_message.deleted", item: TravelBook.model_name.human)
   end
 
   def delete_image
@@ -70,7 +70,7 @@ class TravelBooksController < ApplicationController
     # しおりの作成者でない場合のみ削除
     if user != @travel_book.creator
       @travel_book.users.destroy(user)
-      redirect_to share_travel_book_path(@travel_book), success: "しおりのメンバーから削除しました"
+      redirect_to share_travel_book_path(@travel_book), notice: "しおりのメンバーから削除しました"
     else
       redirect_to share_travel_book_path(@travel_book), alert: "しおりの作成者は削除できません"
     end
