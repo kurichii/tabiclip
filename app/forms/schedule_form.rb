eb528467258d46ed53073a2ccef2b88a24b00ec3
kurichii <ayaka.takahashi830@gmail.com>
2025-04-02 09:11:59 +0900

    本番環境でのデバッグ

--- app/forms/schedule_form.rb
+++ app/forms/schedule_form.rb
@@ -106,10 +106,11 @@ class ScheduleForm
   end
 
   def convert_to_jst
-    self.start_date = Time.zone.parse(start_date.to_s).in_time_zone("Tokyo") if start_date.present?
+    self.start_date = Time.zone.parse(start_date.to_s) if start_date.present?
     Rails.logger.info "================"
-    Rails.logger.info "#{start_date}"
+    Rails.logger.info "#{Time.zone.name}"
+    Rails.logger.info "#{self.start_date}"
     Rails.logger.info "================"
-    self.end_date = Time.zone.parse(end_date.to_s).in_time_zone("Tokyo") if end_date.present?
+    self.end_date = Time.zone.parse(end_date.to_s) if end_date.present?
   end
 end
