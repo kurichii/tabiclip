40fde31c27026d1650b27b253ce7bc2892e91417
kurichii <ayaka.takahashi830@gmail.com>
2025-04-02 15:13:03 +0900

    SchedulesController#create,updateでparamsのタイムゾーンの変換

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -21,9 +21,9 @@ class SchedulesController < ApplicationController
 
   def create
     @schedule_form = ScheduleForm.new(schedule_params)
-    # config.time_zoneで設定されたタイムゾーンに変換
-    @schedule_form.start_date = Time.zone.parse(schedule_params[:start_date]) unless schedule_params[:start_date].nil?
-    @schedule_form.end_date = Time.zone.parse(schedule_params[:end_date]) unless schedule_params[:end_date].nil?
+
+    convert_to_jst(schedule_params[:start_date], schedule_params[:end_date])
+
     if @schedule_form.save
       redirect_to travel_book_schedules_path(@travel_book), notice: t("defaults.flash_message.created", item: Schedule.model_name.human)
     else
@@ -42,6 +42,9 @@ class SchedulesController < ApplicationController
   def update
     @spot = @schedule.spot
     @schedule_form = ScheduleForm.new(schedule_params, schedule: @schedule, spot: @spot)
+
+    convert_to_jst(schedule_params[:start_date], schedule_params[:end_date])
+
     if @schedule_form.update(schedule_params)
       redirect_to travel_book_schedules_path(@travel_book), notice: t("defaults.flash_message.updated", item: Schedule.model_name.human)
     else
@@ -87,5 +90,11 @@ class SchedulesController < ApplicationController
   def set_schedule
     @schedule = Schedule.find(params[:id])
     @travel_book = @schedule.travel_book
+  end
+
+  # config.time_zoneで設定されたタイムゾーンに変換
+  def convert_to_jst(start_date, end_date)
+    @schedule_form.start_date = Time.zone.parse(start_date) unless start_date.nil?
+    @schedule_form.end_date = Time.zone.parse(end_date) unless end_date.nil?
   end
 end
