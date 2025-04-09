8f413ebd0913ac980a2620b4bcbd51967f467905
kurichii <ayaka.takahashi830@gmail.com>
2025-04-09 13:13:08 +0900

    add:ページタイトルを切り替えるメソッドを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def page_title(title = "")
+    base_title = "たびくりっぷ"
+    title.present? ? "#{title} | #{base_title}" : base_title
+  end
+
   def default_meta_tags
     {
       site: "たびくりっぷ",
