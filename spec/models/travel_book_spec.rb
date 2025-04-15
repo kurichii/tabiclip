b8cae248b2ace5fe285261b7f7e00aff5e994dde
kurichii <ayaka.takahashi830@gmail.com>
2025-04-15 11:29:06 +0900

    add:テストケースの記載

--- spec/models/travel_book_spec.rb
+++ spec/models/travel_book_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe TravelBook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションチェック" do
+    it "設定したすべてのバリデーションが機能しているか" do
+    end
+    it "titleがない場合にバリデーションが機能してinvalidになるか" do
+    end
+    it "titleが256文字以上の場合にバリデーションが機能してinvalidになるか" do
+    end
+    it "desctiptionが65536以上の場合にバリデーションが機能してinvalidになるか" do
+    end
+    it "end_dateがstart_dateより過去の場合にバリデーションが機能してinvalidになるか" do
+    end
+    it "titleが255文字以下の場合にバリデーションエラーが起きないか" do
+    end
+    it "descriptionが65535以下の場合にバリデーションエラーが起きないか" do
+    end
+    it "end_dateがstart_dateより未来の場合にバリデーションエラーが起きないか" do
+    end
+  end
 end
