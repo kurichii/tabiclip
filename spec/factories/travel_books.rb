fefd7472a709d34c60e832873bc3bf0f9052eeb4
kurichii <ayaka.takahashi830@gmail.com>
2025-04-15 21:41:20 +0900

    add:factory botでデータ作成

---
+++ spec/factories/travel_books.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :travel_book do
+    title { "title" }
+    is_public { false }
+
+    # オプション
+    description { nil }
+    start_date { nil }
+    end_date { nil }
+    travel_book_image { nil }
+    invitation_token { nil }
+    area { nil }
+    traveler_type { nil }
+
+    # アソシエーション
+    association :creator, factory: :user
+  end
+end
